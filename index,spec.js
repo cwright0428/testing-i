16d4bd269c9da2e4af081468a2b14d2e849afc7a
cwright0428 <cwright0428@yahoo.com>
2019-03-11 16:47:44 -0500

    create test frameworks and item objects

---
+++ index,spec.js
@@ -0,0 +1,19 @@
+describe('repair()', () => {
+    it('should take an item and return a new item with 100 durability', () => {
+
+    })
+})
+
+
+describe('success()', () => {
+    it('should take an item and return a new item with enhanced  paramaters', () => {
+
+    })
+})
+
+describe('fail()', () => {
+    it('should take an item and return a new item with reduced  paramaters', () => {
+
+    })
+})
+
---
+++ index.js
@@ -0,0 +1,7 @@
+
+const item = {
+    name: '',
+    type: '',
+    durability: 100,
+    enhancement: 0
+}
